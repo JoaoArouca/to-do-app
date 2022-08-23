fd2c318e118a41686a1ecc022c21bea4079e9f71
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-08-23 10:42:00 -0300

    feat: create interfaces IUser, ITask

---
+++ back-end/src/interfaces/index.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  id: number,
+  name: string,
+  email: string,
+  password: string,
+}
+
+export interface ITask {
+  id: number,
+  description: string,
+  status: string,
+  user_id: number,
+}
