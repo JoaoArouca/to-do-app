b78f5f1379a65d3e23dfd19e26640b330dc759dd
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-08-24 17:36:11 -0300

    feat(validator) add validator to all userInfo

--- back-end/src/tools/validator.ts
+++ back-end/src/tools/validator.ts
@@ -1,8 +1,13 @@
+import { IUser } from './../interfaces/index';
 import Joi from "joi";
 
 export default class Validator {
-  email (email: string): void {
-    const joi = Joi.string().email().required().validate(email);
+  isValidRegister (payload: IUser): void {
+    const joi = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+      name: Joi.string().required(),
+    }).validate(payload);
 
     if (joi.error) { throw new Error(joi.error.message) }
   }
